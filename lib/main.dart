8d3f7178605b4f6ccfde87db962bb99bbfa5b678
shahbozhoca <shahboz@hoca.dev>
2024-01-30 23:45:10 +0500

    update and add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,27 @@ void main() {
   runApp(const MyApp());
 }
 
+String selectedPage = '';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    // GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
     // double screenWidth = MediaQuery.of(context).size.width;
 
-    return MaterialApp( 
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           elevation: 0,
           titleSpacing: 0,
-          title: Row(
-            children: [
-              Center(
+          leadingWidth: 72,
+          leading: Builder(
+            builder: (context) => InkWell(
+              child: Center(
                 child: Container(
                   color: const Color(0xFF111536),
                   width: 72,
@@ -36,23 +41,123 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              onTap: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ),
+          title: Row(
+            children: [
               SizedBox(
                 height: 60,
                 child: Container(
                   width: 363,
-                  height: 40,
+                  height: 36,
                   padding:
                       const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                   color: const Color(0xFF0D0F23),
                   child: SearchBar(
                     padding: const MaterialStatePropertyAll<EdgeInsets>(
                         EdgeInsets.symmetric(horizontal: 16.0)),
+                    hintText: "Search",
+                    backgroundColor: MaterialStateProperty.all<Color?>(
+                      const Color(0xFFFFFFFF),
+                    ),
                     onTap: () {},
                     onChanged: (_) {},
                     leading: const Icon(Icons.search),
                   ),
                 ),
               )
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          width: 290,
+          backgroundColor: const Color(0xFF0D0F23),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 170,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                decoration: const BoxDecoration(
+                  color: Color(0xFF111536),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.asset(
+                              "assets/images/home.png",
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          "assets/aside/light.svg",
+                          width: 25,
+                          height: 25,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "John Doe",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              "+1 (234) 567 89 01",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SvgPicture.asset(
+                          "assets/aside/arrow.svg",
+                          width: 25,
+                          height: 25,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const DrawerTitle(
+                  imagePath: "assets/aside/phone.svg", title: "Calls"),
+              const DrawerTitle(
+                  imagePath: "assets/aside/history.svg", title: "History"),
+              const DrawerTitle(
+                  imagePath: "assets/aside/heart.svg", title: "Favorites"),
             ],
           ),
         ),
@@ -75,7 +180,7 @@ class MyApp extends StatelessWidget {
             Container(
               width: 363,
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              color: const Color(0xFF111536),
+              color: const Color(0xFF0D0F23),
               child: ListView.builder(
                 scrollDirection: Axis.vertical,
                 itemCount: 20,
@@ -139,6 +244,47 @@ class MainCategory extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class DrawerTitle extends StatelessWidget {
+  final String imagePath;
+  final String title;
+
+  const DrawerTitle({
+    super.key,
+    required this.imagePath,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      hoverColor: const Color(0xFF111536),
+      leading: SvgPicture.asset(
+        imagePath,
+        height: 25,
+        width: 25,
+      ),
+      title: Container(
+        margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+        child: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+        // setState(() {
+        //   selectedPage = 'Settings';
+        // });
+      },
     );
   }
 }
